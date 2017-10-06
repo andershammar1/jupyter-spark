5c710dfc6450e8d45c45f0479bfef06f51c3bcde
Anders Hammar <anders.hammar1@sonymobile.com>
2017-10-06 14:55:20 +0200

    Added link to the Spark Web UI from the Spark job progress name

--- src/jupyter_spark/static/extension.js
+++ src/jupyter_spark/static/extension.js
@@ -13,31 +13,33 @@ var current_update_frequency;
 var spark_is_running = false;
 var cell_queue = [];
 var current_cell;
-var cell_jobs_counter = 0;
 var jobs_in_cache = 0;
 
-var update = function(api_url) {
-    update_cache(api_url, update_dialog_contents);
+var update = function(proxy_url) {
+    update_cache(proxy_url, update_dialog_contents);
 };
 
 // callbacks follows jQuery callback style, can be either single function or array of functions
 // callbacks will be passed the cache as a parameter
-var update_cache = function(api_url, callbacks) {
+var update_cache = function(proxy_url, callbacks) {
     var cbs;
     if (callbacks) {
         cbs = $.Callbacks();
         cbs.add(callbacks);
     }
-    $.getJSON(api_url + '/applications').done(function(applications) {
+    $.getJSON(proxy_url + '/api/v1/applications').done(function(applications) {
         var num_applications = cache.length;
         var num_completed = 0;
         // Check if Spark is running before processing applications
         if(!applications.hasOwnProperty('error')){
             spark_is_running = true;
             applications.forEach(function(application, i) {
-                $.getJSON(api_url + '/applications/' + application.id + '/jobs').done(function (jobs) {
+                $.getJSON(proxy_url + '/api/v1/applications/' + application.id + '/jobs').done(function (jobs) {
                     cache[i] = application;
                     cache[i].jobs = jobs;
+                    cache[i].jobs.forEach(function(job) {
+                        job.url = proxy_url + '/jobs/job?id=' + job.jobId;
+                    });
 
                     num_completed++;
                     if (num_completed === num_applications && cbs) {
@@ -150,7 +152,7 @@ define([
 ], function ($, dialog, events, utils, codecell) {
     var CodeCell = codecell.CodeCell;
     var base_url = utils.get_body_data('baseUrl') || '/';
-    var api_url = base_url + 'spark/api/v1';
+    var proxy_url = base_url + 'spark';
 
     var show_running_jobs = function() {
         var element = $('<div/>').attr('id', 'dialog_contents');
@@ -168,7 +170,7 @@ define([
         var cell = data.cell;
         if (is_spark_cell(cell)) {
             window.clearInterval(current_update_frequency);
-            current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY_ACTIVE, api_url);
+            current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY_ACTIVE, proxy_url);
             cell_queue.push(cell);
             current_cell = cell_queue[0];
             add_progress_bar(current_cell);
@@ -179,14 +181,13 @@ define([
         var progress_bar_div = cell.element.find('.progress-container');
         if (progress_bar_div.length < 1) {
             var input_area = cell.element.find('.input_area');
-            cell_jobs_counter = 0;
             if (spark_is_running) {
                 jobs_in_cache = cache[0].jobs.length;
             };
             var jobs_completed_container = $('<div/>')
                 .addClass('progress_counter')
                 .css({'border': 'none', 'border-top': '1px solid #cfcfcf', 'padding-left': '10px'})
-                .text(PROGRESS_COUNT_TEXT + cell_jobs_counter)
+                .text(PROGRESS_COUNT_TEXT + 0 + ": ")
                 .hide();
             var progress_bar_container = $('<div/>')
                 .addClass('progress-container')
@@ -224,13 +225,14 @@ define([
         if (progress_count.length < 1) {
             console.log("No progress counter found");
         };
-        var job_name = "";
         if (spark_is_running) {
-            cell_jobs_counter = cache[0].jobs.length - jobs_in_cache;
-            job_name =  ": " + cache[0].jobs[0].name
+            progress_count.text(PROGRESS_COUNT_TEXT + cache[0].jobs[0].jobId + ': ');
+            var job_name_link = $('<a target="_blank"></a>')
+                .attr('href', cache[0].jobs[0].url)
+                .text(cache[0].jobs[0].name);
+            progress_count.append(job_name_link);
         };
 
-        progress_count.text(PROGRESS_COUNT_TEXT + cell_jobs_counter + job_name);
         progress_count.show();
     };
 
@@ -255,7 +257,7 @@ define([
             add_progress_bar(current_cell);
         } else {
             window.clearInterval(current_update_frequency);
-            current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY, api_url);
+            current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY, proxy_url);
         };
     };
 
@@ -279,8 +281,8 @@ define([
             'callback': show_running_jobs,
             'id': 'show_running_jobs'
         }]);
-        update(api_url);
-        current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY, api_url);
+        update(proxy_url);
+        current_update_frequency = window.setInterval(update, UPDATE_FREQUENCY, proxy_url);
     };
 
     return {
